40b144feb1b5067433d2579efad9ac9d77f039d2
bobo <980742324@qq.com>
2019-03-09 23:43:48 +0800

    完成新增标签界面

--- src/views/mine/tags.vue
+++ src/views/mine/tags.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="topic-container">
-    <h1>浏览话题</h1>
+  <div class="app-container">
+    <div class="title-container">
+      <h2>我的标签</h2>
+    </div>
+    <el-row>
+      <el-col :span="12">
+        <h3>标签列表</h3>
+      </el-col>
+      <el-col :span="12">
+        <h3>新增标签</h3>
+        <el-form ref="form" :model="newtagform">
+          <el-form-item label="标签名：">
+            <el-input v-model="newtagform.name"/>
+          </el-form-item>
+          <el-form-item label="文字颜色：">
+            <el-color-picker v-model="newtagform.color"/>
+          </el-form-item>
+          <el-form-item label="背景颜色：">
+            <el-color-picker v-model="newtagform.background"/>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">立即创建</el-button>
+          </el-form-item>
+        </el-form>
+        <div>
+          <h3>效果预览</h3>
+          <div class="preview-content">
+            <el-tag :color="newtagform.background" :style="{ color: newtagform.color }" :hit="true">{{ previewtxt }}</el-tag>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Topic',
+  name: 'MineTags',
   data() {
-    return {}
+    return {
+      newtagform: {
+        name: '', // 标签名
+        color: '#669EFF', // 标签文字颜色
+        background: '#ECF5FF' // 标签背景颜色
+      }
+    }
+  },
+  computed: {
+    previewtxt() {
+      return this.newtagform.name.length > 0 ? this.newtagform.name : '示例标签'
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../../styles/variables.scss';
 
+.title-container {
+  border-bottom: 1px solid $oneBorder;
+  color: $mainTxt;
+}
+
+.preview-container {
+  border-top: 1px dashed $twoBorder;
+}
+
+.preview-content {
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: $twoBorder;
+  border-radius: 4px;
+}
 </style>
